19477e7a83f93f5d13eaa9185be5a3825406ff71
Minjae Kim <137mjkim@gmail.com>
2026-07-07 13:37:44 +0900

    실습 3-4: @RequiredArgsConstructor, @AllArgsConstructor, @NoArgsConstructor 실습

--- src/main/java/com/example/demo/member/Member.java
+++ src/main/java/com/example/demo/member/Member.java
@@ -6,11 +6,14 @@ import lombok.*;
 @ToString(of = { "name", "email" }) // 클래스 단위의 ToString에서 이 필드들만 표기하고 싶을 때
 // @ToString(callSuper = true) // 부모클래스의 필드들도 ToString에 표기하고 싶을 때
 @Getter
-@AllArgsConstructor(access = AccessLevel.PUBLIC)
+// @AllArgsConstructor(access = AccessLevel.PUBLIC)
+@RequiredArgsConstructor
 public class Member {
     private Integer id;
     // @ToString.Exclude: 클래스 단위의 ToString에서 해당 필드를 제외하고 싶을 때
-    private String name;
+    private final String name;
+        // "final" 키워드가 있으면 그건 필요한 필드이다
+        // 나중에 설정해도 되는건 @Setter 붙이기
     @Setter(AccessLevel.PRIVATE)
     private int age;
     @Setter
