b97993d1b01b6c900161fa878e41184327730a46
Minjae Kim <137mjkim@gmail.com>
2026-07-07 13:15:00 +0900

    실습 3-3: @ToString 실습 (exclude, of, callSuper)

--- src/main/java/com/example/demo/member/Member.java
+++ src/main/java/com/example/demo/member/Member.java
@@ -1,14 +1,15 @@
 package com.example.demo.member;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+// @ToString(exclude = "email") : 클래스 단위의 ToString에서 해당 필드를 제외하고 싶을 때
+@ToString(of = { "name", "email" }) // 클래스 단위의 ToString에서 이 필드들만 표기하고 싶을 때
+// @ToString(callSuper = true) // 부모클래스의 필드들도 ToString에 표기하고 싶을 때
 @Getter
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class Member {
     private Integer id;
+    // @ToString.Exclude: 클래스 단위의 ToString에서 해당 필드를 제외하고 싶을 때
     private String name;
     @Setter(AccessLevel.PRIVATE)
     private int age;
