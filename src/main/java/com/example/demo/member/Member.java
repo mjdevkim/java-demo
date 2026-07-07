92f0c4365e5d2d487482a1606292695dd4235492
Minjae Kim <137mjkim@gmail.com>
2026-07-07 15:25:45 +0900

    실습 3-6: @FieldDefaults: 필드에 대한 접근제어자 일괄 적용 실습

--- src/main/java/com/example/demo/member/Member.java
+++ src/main/java/com/example/demo/member/Member.java
@@ -1,6 +1,7 @@
 package com.example.demo.member;
 
 import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 // @ToString(exclude = "email") : 클래스 단위의 ToString에서 해당 필드를 제외하고 싶을 때
 @ToString(of = { "name", "email" }) // 클래스 단위의 ToString에서 이 필드들만 표기하고 싶을 때
@@ -11,12 +12,13 @@ import lombok.*;
 // @EqualsAndHashCode(of = {"id", "name"}) // 비교에 "쓸" 필드
 // @EqualsAndHashCode(exclude = {"email"}) // 비교에 "제외할" 필드
 // @EqualsAndHashCode(callSuper = true) // 상속한 클래스 필드 포함하여 동등성 판단
+@FieldDefaults(makeFinal = false, level = AccessLevel.PRIVATE)
 public class Member {
-    private Integer id;
+    Integer id;
     // @ToString.Exclude: 클래스 단위의 ToString에서 해당 필드를 제외하고 싶을 때
-    private String name;
-    @Setter(AccessLevel.PRIVATE)
-    private int age;
+    String name;
+    @Setter(AccessLevel.PUBLIC)
+    int age;
     @Setter
-    private String email;
+    String email;
 }
