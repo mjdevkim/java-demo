af5049b9e9cdfa3589f3d3f8cbd2edde0b6ec987
Minjae Kim <137mjkim@gmail.com>
2026-07-08 11:29:39 +0900

    실습 4-4: static class 실습 (개념은 이해하고 있어야)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,11 +6,18 @@ import java.util.Arrays;
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
-		double myRate = TaxCalculator.calculateTax(2500);
-		System.out.println(myRate);
+		Investment investment = new Investment();
 
-		// 예시: Arrays 클래스의 .sort() 메소드도 Static 메소드다.
-		int[] nums = {1,2,3};
-		Arrays.sort(nums);
+		// investment 객체에서 Stock 클래스의 객체를 new Stock();으로 만든것
+//		Investment.Stock stockInvestment = investment.new Stock();
+
+		Investment.Stock stockInvestment = new Investment.Stock();
+		// estimated()가 객체 메서드이기 떄문에 객체 initiate을 해야 한다.
+		// stockInvestment.estimated();
+
+		// estimated()가 클래스 메서드라면
+		Investment.Stock.estimated();
+
+
 	}
 }
